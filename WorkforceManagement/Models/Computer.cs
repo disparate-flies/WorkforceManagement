b9ca2a2c9150b154f99819742d280ad26e8b1560
AmandaM04 <wreedamitchell@gmail.com>
2018-09-27 14:27:51 -0500

    created the Computer Model

---
+++ WorkforceManagement/Models/Computer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkforceManagement.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public string Model { get; set; }
+
+        public DateTime DecommissionDate { get; set; }
+
+        public string Condition { get; set; }
+    }
+}
