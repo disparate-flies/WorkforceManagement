732ca15d586fce37bf6400a84101d9826d65b25f
AmandaM04 <wreedamitchell@gmail.com>
2018-09-27 14:52:55 -0500

    created Computer Create View Model

---
+++ WorkforceManagement/Models/ComputerCreateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkforceManagement.Models.ViewModels
+{
+    public class ComputerCreateViewModel
+    {
+        public Computer Computer { get; set; }
+
+        //private readonly IConfiguration _config;
+
+        //public IDbConnection Connection
+        //{
+        //    get
+        //    {
+        //        return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        //    }
+        // }
+    }
+}
