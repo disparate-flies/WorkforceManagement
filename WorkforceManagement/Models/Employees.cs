d172d4d61fb47bde751ae49daa240926f4563dbb
Joey <smithjoey5123@gmail.com>
2018-09-27 14:55:40 -0500

    Employee Model

---
+++ WorkforceManagement/Models/Employees.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkforceManagement.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Is Supervisor")]
+        public bool IsSupervisor { get; set; }
+
+        [Required]
+        [Display(Name = "Department Id")]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [Display(Name = "Is Active")]
+        public bool IsActive { get; set; }
+
+        [Required]
+        [Display(Name = "Currently Assigned Computer")]
+        public int ComputerId { get; set; }
+
+        public List<TrainingProgram> TrainingPrograms { get; set; } = new List<TrainingProgram>();
+    }
+}
