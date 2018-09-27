f7c9ed2d41e1607eb45fd8732c3234f7a958ca7e
Robert Leedy <leedyrj@gmail.com>
2018-09-27 15:36:00 -0500

    created departments model

---
+++ WorkforceManagement/Models/Departments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkforceManagement.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+        public double ExpenseBudget { get; set; }
+        public List<Employee> EmployeeList { get; set; } = new List<Employee>();
+    }
+}
