926da6523bf9329c615da047411e74d9d4f58930
AmandaM04 <wreedamitchell@gmail.com>
2018-09-28 11:00:58 -0500

    added required fields and data annotations

--- WorkforceManagement/Models/Computer.cs
+++ WorkforceManagement/Models/Computer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,23 @@ namespace WorkforceManagement.Models
     {
         public int Id { get; set; }
 
-        public DateTime PurchaseDate { get; set; } 
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Purchase Date")]
+        public DateTime PurchaseDate { get; set; }
 
-        public string Model { get; set; }
+        [Required]
+        public string Manufacturer { get; set; }
 
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Decommission Date")]
         public DateTime DecommissionDate { get; set; }
 
+        [Required]
         public string Condition { get; set; }
     }
 }
