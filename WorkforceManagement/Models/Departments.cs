a962ea5cab47f62fa291b8276aaf7f0d1349f80b
Robert Leedy <leedyrj@gmail.com>
2018-09-28 10:11:48 -0500

    updated pull request

--- WorkforceManagement/Models/Departments.cs
+++ WorkforceManagement/Models/Departments.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace WorkforceManagement.Models
     public class Department
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Department Name")]
         public string DeptName { get; set; }
+
+        [Required]
+        [Display(Name = "Department Budget")]
         public double ExpenseBudget { get; set; }
+
         public List<Employee> EmployeeList { get; set; } = new List<Employee>();
     }
 }
