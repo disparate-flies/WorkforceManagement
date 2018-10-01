619060f1c30940ed7043b968898a2edc869d2ff3
Adelaide <adelaide.yoder@gmail.com>
2018-10-01 10:02:17 -0500

    merged master

--- WorkforceManagement/Controllers/EmployeeController.cs
+++ WorkforceManagement/Controllers/EmployeeController.cs
@@ -4,15 +4,9 @@ using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.Data.Sqlite;
-using Microsoft.Extensions.Configuration;
-using Workforce.Models;
-using Workforce.Models.ViewModels;
-using System.Data.SqlClient;
+
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WorkforceManagement.Controllers
