600d1230635ad49cdd99f0c978540ad45bdbdee6
Robert Leedy <leedyrj@gmail.com>
2018-10-02 14:23:36 -0500

    fixed merge issue

--- WorkforceManagement/Controllers/EmployeeController.cs
+++ WorkforceManagement/Controllers/EmployeeController.cs
@@ -6,16 +6,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
 using Microsoft.AspNetCore.Mvc.Rendering;
-//using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 using WorkforceManagement.Models;
-//using WorkforceManagement.Models.ViewModels;
 using System.Data.SqlClient;
-=======
 
->>>>>>> master
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WorkforceManagement.Controllers
