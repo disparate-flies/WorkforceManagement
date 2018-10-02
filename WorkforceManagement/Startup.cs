23968917638681bacde8d34003c656d2a6c2b70e
Adelaide <adelaide.yoder@gmail.com>
2018-10-02 17:20:01 -0500

    commented out the //app.UseHttpsRedirection();

--- WorkforceManagement/Startup.cs
+++ WorkforceManagement/Startup.cs
@@ -48,7 +48,7 @@ namespace WorkforceManagement
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
