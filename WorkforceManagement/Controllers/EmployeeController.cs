b6a1fd15ffacee8dca4466f2d30b209ee325f783
Joey <smithjoey5123@gmail.com>
2018-10-03 15:36:52 -0500

    Employees view, view details, and add complete

--- WorkforceManagement/Controllers/EmployeeController.cs
+++ WorkforceManagement/Controllers/EmployeeController.cs
@@ -10,11 +10,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Configuration;
 using WorkforceManagement.Models;
 using System.Data.SqlClient;
-<<<<<<< HEAD
 using Dapper;
-=======
->>>>>>> master
-
+using WorkforceManagement.Models.ViewModels;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WorkforceManagement.Controllers
@@ -68,7 +65,7 @@ namespace WorkforceManagement.Controllers
             }
         }
 
-        public async Task<IActionResult>Details(int? id)
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
@@ -87,41 +84,55 @@ namespace WorkforceManagement.Controllers
                    ec.ComputerId,
                    c.Id,
                    c.Make,
-                   c.Manufacturer,
-                   et.EmployeeId,
-                   et.TrainingProgramId,
-                   tp.Id,
-                   tp.ProgName
+                   c.Manufacturer
               from Employee e
               join Department d on e.DepartmentId = d.Id
-              join EmployeeComputer ec on e.Id = ec.EmployeeId
-              join Computer c on c.Id = ec.ComputerId
-              join EmployeeTraining et ON et.EmployeeId = e.Id
-              join TrainingProgram tp ON tp.ID = et.TrainingProgramId
+              left join EmployeeComputer ec on e.Id = ec.EmployeeId
+              left join Computer c on c.Id = ec.ComputerId
               where e.Id = {id}";
 
             using (IDbConnection conn = Connection)
             {
-                Employees employees = (await conn.QueryAsync<Employees>(sql)).ToList().Single();
+                Employees emp = new Employees();
+                Department dept = new Department();
+                Computer comp = new Computer();
+                var employeeQuerySet = await conn.QueryAsync<Employees, Department, Computer, Employees>(
+    sql,
+             (employee, department, computer) =>
+             {
 
-                if (employees == null)
-                {
-                    return NotFound();
-                }
+                 emp.Id = employee.Id;
+                 emp.FirstName = employee.FirstName;
+                 emp.LastName = employee.LastName;
 
-                return View(employees);
-            }
+                 dept.Id = department.Id;
+                 dept.DeptName = department.DeptName;
+
+                 if (computer != null)
+                 {
+                     comp.Id = computer.Id;
+                     comp.Manufacturer = computer.Manufacturer;
+                 }
+
+
+                 return employee;
+           });
+                emp.Department = dept;
+                emp.Computer = comp;
+                return View(emp);
+             }
         }
 
-        public async Task<SelectList> EmployeeList(int? selected)
+ 
+        public async Task<SelectList> DepartmentList(int? selected)
         {
             using (IDbConnection conn = Connection)
             {
-                List<Employees> employees = (await conn.QueryAsync<Employees>("SELECT Id, FirstName FROM Employee")).ToList();
+                List<Department> departments = (await conn.QueryAsync<Department>("SELECT Id, DeptName FROM Department")).ToList();
 
-                employees.Insert(0, new Employees() { Id=0, FirstName="Insert New Employee..."});
+                departments.Insert(0, new Department() { Id=0, DeptName = "Select Department..."});
 
-                var selectList = new SelectList(employees, "Id", "FirstName", selected);
+                var selectList = new SelectList(departments, "Id", "Name", selected);
                 return selectList;
             }
         }
@@ -130,26 +141,27 @@ namespace WorkforceManagement.Controllers
         {
             using (IDbConnection conn = Connection)
             {
-                ViewData["EmployeeId"] = await EmployeeList(null);
-                return View();
+                EmployeesCreateViewModel model = new EmployeesCreateViewModel(_config);
+
+                return View(model);
             }
         }
 
         // POST: Employee/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create (Employees employees)
+        public async Task<IActionResult> Create (EmployeesCreateViewModel employeescreateviewmodel)
         {
             if (ModelState.IsValid)
             {
                 string sql = $@"
                     insert into Employee
-                        (FirstName, LastName, StartDate, Department)
+                        (FirstName, LastName, StartDate, DepartmentId)
                     values
-                        ('{employees.FirstName}',
-                         '{employees.LastName}',
-                         '{employees.StartDate}',   
-                         '{employees.Department.Id}')";
+                        ('{employeescreateviewmodel.Employees.FirstName}',
+                         '{employeescreateviewmodel.Employees.LastName}',
+                         '{employeescreateviewmodel.Employees.StartDate}',   
+                         '{employeescreateviewmodel.DepartmentId}')";
 
                 using (IDbConnection conn = Connection)
                 {
@@ -160,14 +172,9 @@ namespace WorkforceManagement.Controllers
                         return RedirectToAction(nameof(Index));
                     }
                 }
-            }
 
-            using (IDbConnection conn = Connection)
-            {
-                IEnumerable<Employees> employee = (await conn.QueryAsync<Employees>("SELECT Id, FirstName FROM Employees")).ToList();
-                ViewData["EmployeeId"] = await EmployeeList(employees.Id);
-                return View(employee);
             }
+            return View(employeescreateviewmodel);
         } 
     }
 }
